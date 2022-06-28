4d9a85fd773bbbfee5e1217fa1fd5779d4fd42b6
lestercoyoyjr <lester_cch@hotmail.com>
2022-06-27 22:50:17 -0600

    try-catch improvement

--- src/controllers/language.controller.js
+++ src/controllers/language.controller.js
@@ -5,10 +5,15 @@ import { getConnection } from "./../database/database";
 // we must wait a time so they can load
 // we use "async" and "await"
 const getLanguages = async(req,res)=>{
-    const connection = await getConnection();
-    const result =  await connection.query("SELECT id, name, programmers FROM language");
-    console.log(result);
-    res.json(result);
+    try {
+        const connection = await getConnection();
+        const result =  await connection.query("SELECT id, name, programmers FROM language");
+        console.log(result);
+        res.json(result);   
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
 };
 
 export const methods={
